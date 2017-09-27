a40957a6de954ef550d43d30accafd85b76e63e1
zhishaofei <zhishaofei@vipkid.com.cn>
2017-09-27 17:22:46 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,29 @@
-import * as x1 from './fileWithoutError';
-import * as x2 from './fileWithError';
+// import * as x1 from './fileWithoutError';
+// import * as x2 from './fileWithError';
 import * as _ from "lodash";
+import Sprite = Laya.Sprite;
 
-let result = x1 + x2;
+module laya {
+	export class GameMain {
+		private container: Sprite;
+		private socket: any;
 
-console.log(_.random);
-console.log(_.random(0, 100));
+		constructor() {
+			Laya.init(600, 400);
+			this.initDraw()
+		}
 
-console.log('result34', result);
-export = result;
+		private initDraw(): void {
+			this.container = new Sprite();
+			Laya.stage.addChild(this.container);
+
+
+			var sp = new Sprite();
+			//画圆
+			sp.graphics.drawCircle(80, 80, _.random(0, 300), "#ff0000");
+			this.container.addChild(sp);
+		}
+	}
+}
+new laya.GameMain();
+
